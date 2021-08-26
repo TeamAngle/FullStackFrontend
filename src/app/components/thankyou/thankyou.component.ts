e9247b9a69c50ff59d803016f23d3c99a0d9c48f
Zach <zach@zipcodes-MacBook-Pro-6.local>
2021-08-25 21:51:37 -0400

    Added thank you page, placeholder message until final message decided. Imported header, component ready for styling

---
+++ src/app/components/thankyou/thankyou.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { HeaderComponent } from '../header/header.component';
+
+@Component({
+  selector: 'app-thankyou',
+  templateUrl: './thankyou.component.html',
+  styleUrls: ['./thankyou.component.css']
+})
+export class ThankyouComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
