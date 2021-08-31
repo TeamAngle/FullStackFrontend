de08171a7257233d69fe95f88d69fe714eb4de28
jgiroso <jrgiroso@gmail.com>
2021-08-31 15:55:40 -0400

    heroku testing

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,3 @@ export const environment = {
   production: true,
   apiBaseUrl: 'http://localhost:8080'
 };
-
-const API = process.env.NODE_ENV === 'production' ? 'https://sublime-kitchen.herokuapp.com/' : 'http://localhost:8080';
-
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,8 +7,6 @@ export const environment = {
   apiBaseUrl: 'http://localhost:8080'
 };
 
-const API = process.env.NODE_ENV === 'production' ? 'https://sublime-kitchen.herokuapp.com/' : 'http://localhost:8080';
-
 
 /*
  * For easier debugging in development mode, you can import the following file
