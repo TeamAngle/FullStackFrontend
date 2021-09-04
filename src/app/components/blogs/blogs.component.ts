9443a8ca5f591306b8b42daa562b1fd3d31b2c65
Zach <zach@zipcodes-MacBook-Pro-6.local>
2021-09-04 14:35:59 -0400

    refactored blogs componenet to be a list of all blogs available to the website. Each blog title has a routerlink that, when clicked, leads the user to that particular blog and all of it's contents

--- src/app/components/blogs/blogs.component.ts
+++ src/app/components/blogs/blogs.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { BlogPost } from 'src/app/blogPost';
+import { BlogPostService } from 'src/app/services/blog-post.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-blogs',
@@ -6,10 +9,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./blogs.component.css']
 })
 export class BlogsComponent implements OnInit {
-
-  constructor() { }
+  apiServerUrl = "localhost:4200"
+  blogs:BlogPost[];
+  constructor(private blogPostService: BlogPostService) { }
 
   ngOnInit(): void {
+    this.getAllBlogs();
   }
 
+  getAllBlogs(){
+    this.blogPostService.getBlogPosts().subscribe(
+      blogPosts => this.blogs = blogPosts
+    )
+  }
 }
