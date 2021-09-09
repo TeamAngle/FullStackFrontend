4f43f8ba4b5a7dd530441678c917d2f785014492
jgiroso <jrgiroso@gmail.com>
2021-09-09 17:24:04 -0400

    test 2

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,8 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'http://localhost:8080'
+  // apiBaseUrl: 'http://localhost:8080'
   // apiBaseUrl: 'https://sublime-kitchen.herokuapp.com/'
+  apiBaseUrl: 'mysql://bc94f0edc77a52:1bc1ebce@us-cdbr-east-04.cleardb.com/heroku_c6842258982129d?reconnect=true'
 };
 
 
