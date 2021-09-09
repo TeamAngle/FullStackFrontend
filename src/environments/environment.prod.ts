2d97dd7b54e9e2c3039b16c072e65bb30921daec
Zach <zach@zipcodes-MacBook-Pro-6.local>
2021-09-09 14:53:48 -0400

    Attempting to set production environment apiBaseUrl for heroku app

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'http://localhost:8080'
-  // apiBaseUrl: 'https://sublime-kitchen.herokuapp.com/'
+  //apiBaseUrl: 'http://localhost:8080'
+   apiBaseUrl: 'https://sublime-kitchen.herokuapp.com/'
 };
 
 
